c2129d8d00d2de291e90213a36e347ac58fe64a0
Sampramudana <32828518+Sampramudana@users.noreply.github.com>
2017-11-08 11:31:44 +0700

    ProjectQuizKomoditasMakanan

---
+++ KomoditasMakanan/Loan.swift
@@ -0,0 +1,18 @@
+//
+//  Loan.swift
+//  KomoditasMakanan
+//
+//  Created by Sam Pramudana on 11/8/17.
+//  Copyright © 2017 Sam Pramudana. All rights reserved.
+//
+
+import Foundation
+class Loan {
+    
+    var komoditas: String = ""
+    var unit: String = ""
+    var mangkukna: String = ""
+    var kamari: String = ""
+    var ayena: String = ""
+    
+}
---
+++ KomoditasMakanan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KomoditasMakanan
+//
+//  Created by Sam Pramudana on 11/8/17.
+//  Copyright © 2017 Sam Pramudana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
